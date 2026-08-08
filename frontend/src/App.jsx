a1aaf6f007c8241db065237fc0115a490a1dca07
10tanishkagophane-boop <10tanishkagophane@gmail.com>
2026-08-09 00:23:54 +0530

    Resolve merge conflicts and fix frontend build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import PatientCheckin from './pages/PatientCheckin'
-import QueueDashboard from './pages/queuedashboard'
+import QueueDashboard from './pages/QueueDashboard'
 
 function App() {
   return (
